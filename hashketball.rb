a50cb07e956ff3f710acf6809388e14a2ee61a00
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 21:46:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,9 +123,16 @@ def num_points_scored(player_name)
   game_hash.each do |locations, team_data|
     team_data[:players].each do |player, stats|
       if player == player_name
-        return player
+        return stats[:points]
       end
     end
+  end
+end
+
+def shoe_size(player_name)
+  game_hash.each do |locations, team_data|
+    team_data[:players]
+    
   end
   
 end
